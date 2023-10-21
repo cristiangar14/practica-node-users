fdb04eb3209dbbdade8b819b8a0db92dc7e8076e
Cristian Garzon <desarrollo.cristian.garzon@gmail.com>
2023-10-20 20:42:06 -0500

    Feat(add): add jwt validation

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,7 @@ import userUpdateDataDTO from "#Dto/user-update-data.dto.js";
 import userUpdateEmailDTO from "#Dto/user-update-email.dto.js";
 import userUpdatePasswordDTO from "#Dto/user-update-password.dto.js";
 import userUnregisterDTO from "#Dto/user-unregister.dto.js";
+import userJWTDTO from "#Dto/user.jwt.dto.js";
 
 const userRouter = Router();
 
@@ -13,14 +14,14 @@ userRouter.post('/register', userRegisterDTO);
 
 userRouter.post('/login', userLoginDTO);
 
-userRouter.get('/profile');
+userRouter.get('/profile', userJWTDTO);
 
-userRouter.patch('/update-data', userUpdateDataDTO);
+userRouter.patch('/update-data', userJWTDTO, userUpdateDataDTO);
 
-userRouter.patch('/update-email', userUpdateEmailDTO);
+userRouter.patch('/update-email', userJWTDTO, userUpdateEmailDTO);
 
-userRouter.patch('/update-password', userUpdatePasswordDTO);
+userRouter.patch('/update-password', userJWTDTO, userUpdatePasswordDTO);
 
-userRouter.delete('/unregister', userUnregisterDTO);
+userRouter.delete('/unregister', userJWTDTO, userUnregisterDTO);
 
 export default userRouter;
